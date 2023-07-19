f5f4474363ca2d1dfab605a1eaf2603ffa3f3d6f
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-07-18 21:38:28 -0400

    add ghaph algorithm

---
+++ src/graph.py
@@ -0,0 +1,52 @@
+class Vertex:
+    ID: int
+    vertices: list['Vertex']
+
+    def __init__(self, ID: int):
+        self.ID = ID
+        self.vertices = []
+
+    def __get_vertex_id(self, vertex: 'Vertex'):
+        return vertex.ID
+
+    def __str__(self) -> str:
+        ids = list(map(self.__get_vertex_id, self.vertices))
+        return f'{self.ID} - {ids}'
+
+class Graph:
+    size: int
+    length: int
+    vertices: list[Vertex]
+
+    def __init__(self, size: int):
+        self.size = size
+        self.length = 0
+        self.vertices = [None] * size
+
+    def add_vertex(self, ID: int):
+        if self.length == self.size:
+            return
+        new_vertex = Vertex(ID)
+        self.vertices[self.length] = new_vertex
+        self.length += 1
+
+    def get_vertex(self, ID: int):
+        for vertex in self.vertices:
+            if vertex.ID == ID:
+                return vertex
+        return None
+
+    def add_edge(self, source: Vertex, target: Vertex):
+        for vertex in source.vertices:
+            if vertex.ID == target.ID:
+                return
+        source.vertices.append(target)
+
+    def remove_edge(self, ID: int):
+        if self.length == 0:
+            return
+        self.vertices = list(filter(lambda vertex: vertex.ID == ID, self.vertices))
+
+    def print_vertices(self):
+        for vertex in self.vertices:
+            print(vertex)
